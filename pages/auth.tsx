b75a0af4b2776d5140a066e130b2ec246c2ac4b4
vasconcelos1898 <55930398+vasconcelos-gabriel@users.noreply.github.com>
2023-03-28 11:07:46 -0300

    login e register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,16 @@
 import Input from '@/components/Input'
 import { useCallback, useState } from 'react'
+import axios from 'axios'
+import {signIn} from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import {FcGoogle} from 'react-icons/fc'
+import {FaGithub} from 'react-icons/fa'
 
 const Auth = () => {
+  const router = useRouter()
   const [email, setEmail] = useState('')
-  const [userName, setUserName] = useState('')
+  const [name, setName] = useState('')
   const [password, setPassword] = useState('')
 
   const [variant, setVariant] = useState('login')
@@ -13,6 +20,35 @@ const Auth = () => {
       currentVariant === 'login' ? 'register' : 'login'
     )
   }, [])
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      })
+
+      router.push('/')
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }, [email, password, router])
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      })
+      login()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [email, name, password, login])
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -29,9 +65,9 @@ const Auth = () => {
               {variant === 'register' && (
                 <Input
                   label="Nome de usuário"
-                  onChange={(e: any) => setUserName(e.target.value)}
-                  id="userName"
-                  value={userName}
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  value={name}
                 />
               )}
 
@@ -50,12 +86,26 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={variant === 'login' ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === 'login' ? 'Entrar' : 'Cadastrar'}
             </button>
+            <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+              <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer
+              hover:opacity-80 transition'>
+                <FcGoogle size={30} />
+              </div>
+              <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer
+              hover:opacity-80 transition'>
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
-              
-              {variant === 'login' ? 'Primeira vez usando Netflix?' : 'Já possui cadastro?'}
+              {variant === 'login'
+                ? 'Primeira vez usando Netflix?'
+                : 'Já possui cadastro?'}
               <span
                 onClick={toogleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
---
+++ pages/testando.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testando = () => {
+  return (
+    <div className='text-white'>testando</div>
+  )
+}
+
+export default testando
