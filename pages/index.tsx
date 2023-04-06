7907a6f6704ec17e1284f80982545eda9cedcde2
vasconcelos1898 <55930398+vasconcelos-gabriel@users.noreply.github.com>
2023-04-06 14:25:41 -0300

    Ajustes finais

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import Billboard from '@/components/Billboard'
+import InfoModal from '@/components/InfoModal'
+import MovieList from '@/components/MovieList'
 import Navbar from '@/components/Navbar'
 import useCurrentUser from '@/hooks/useCurrentUser'
+import useFavorites from '@/hooks/useFavorites'
+import useInfoModalStore from '@/hooks/useInfoModalStore'
+import useMovieList from '@/hooks/useMovieList'
 import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
 
@@ -20,7 +26,18 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  return <>
-  <Navbar />
-  </>
+  const { data: movies = []} = useMovieList()
+  const { data: favorites = []} = useFavorites()
+  const {isOpen, closeModal} = useInfoModalStore();
+  return (
+    <>
+    <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title='Bombando' data={movies} />
+        <MovieList title='Minha lista' data={favorites} />
+      </div>
+    </>
+  )
 }
